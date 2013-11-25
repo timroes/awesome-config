19530288f8ad693c88797d414c628ed028fa8dfa
Tim Roes <mail@timroes.de>
2013-11-25 21:23:24 +0100

    Find primary monitor

--- rc.lua
+++ rc.lua
@@ -49,7 +49,19 @@ dofile(configpath .. "functions.lua")
 
 -- Set constants
 MOD = "Mod4"
-PRIMARY = 1
+
+-- Find primary screen
+local xrandr = awful.util.pread("xrandr | grep 'connected primary' | cut -f1 -d' '")
+if #xrandr > 0 then
+	-- if a primary screen has been configured via xrandr, use this as primary
+	xrandr = xrandr:gsub("%s+$", "") -- remove newline at end of string
+	PRIMARY = screen[xrandr].index
+end
+-- If xrandr has not been set (or the screen couldn't be detected)
+-- use the screen that is the most in the center of all screens
+if not PRIMARY then
+	PRIMARY = screen_index(math.ceil(screen.count() / 2))
+end
 
 -- Clear all shortcuts before including any config files
 root.keys({ })
