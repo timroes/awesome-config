02f73a97721b3293275191a5208206ca29f3b8af
Tim Roes <mail@timroes.de>
2014-11-02 10:59:00 +0100

    feat: add margin around icons in tasklist

--- conf.d/99bar.lua
+++ conf.d/99bar.lua
@@ -46,11 +46,87 @@ local filter_noname_tags = function(t)
 	return t.name:len() <= 1
 end
 
+-- Copied from awful.widget.common but modified. --
+
+local create_buttons = function(buttons, object)
+	if buttons then
+		local btns = {}
+		for kb, b in ipairs(buttons) do
+			-- Create a proxy button object: it will receive the real
+			-- press and release events, and will propagate them the the
+			-- button object the user provided, but with the object as
+			-- argument.
+			local btn = button { modifiers = b.modifiers, button = b.button }
+			btn:connect_signal("press", function () b:emit_signal("press", object) end)
+			btn:connect_signal("release", function () b:emit_signal("release", object) end)
+			btns[#btns + 1] = btn
+		end
+
+		return btns
+	end
+end
+
+-- Function to render one item in the task list
+local tasklist_widget = function(w, buttons, label, data, objects)
+	-- update the widgets, creating them if needed
+	w:reset()
+	for i, o in ipairs(objects) do
+		local cache = data[o]
+		local ib, tb, bgb, m, l, spacer
+		if cache then
+			ib = cache.ib
+			tb = cache.tb
+			bgb = cache.bgb
+			m   = cache.m
+			im = cache.im
+		else
+			ib = wibox.widget.imagebox()
+			tb = wibox.widget.textbox()
+			bgb = wibox.widget.background()
+			m = wibox.layout.margin(tb, 4, 4)
+			im = wibox.layout.margin(ib, 4, 4, 4, 4)
+			l = wibox.layout.fixed.horizontal()
+
+			-- All of this is added in a fixed widget
+			l:fill_space(true)
+			l:add(im)
+			l:add(m)
+
+			-- And all of this gets a background
+			bgb:set_widget(l)
+
+			bgb:buttons(create_buttons(buttons, o))
+
+			data[o] = {
+				ib = ib,
+				tb = tb,
+				bgb = bgb,
+				m = m,
+				im = im
+			}
+		end
+
+		local text, bg, bg_image, icon = label(o)
+		-- The text might be invalid, so use pcall
+		if not pcall(tb.set_markup, tb, text) then
+			tb:set_markup("<i>&lt;Invalid text&gt;</i>")
+		end
+		bgb:set_bg(bg)
+		if type(bg_image) == "function" then
+			bg_image = bg_image(tb,o,m,objects,i)
+		end
+		bgb:set_bgimage(bg_image)
+		ib:set_image(icon)
+
+		-- spacer = wibox.layout.margin(bgb, 0, 0)
+		w:add(bgb)
+	end
+	-- w:add(wibox.layout.fixed.horizontal())
+end
+
 for s = 1, screen.count() do
 
-	taglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist.buttons)
-
-	tasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist.buttons)
+	tasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist.buttons, nil, tasklist_widget)
 
 	local layoutbox = awful.widget.layoutbox(s)
 
