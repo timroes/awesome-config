b0ef4b12600a4c478064382f47c07ed88e4d8b15
Tim Roes <mail@timroes.de>
2016-05-05 15:04:16 +0200

    set wallpaper on all screens

--- conf.d/30wallpaper.lua
+++ conf.d/30wallpaper.lua
@@ -2,4 +2,7 @@ local gears = require("gears")
 local theme = require("lunaconf.theme")
 local config = require("lunaconf.config")
 
-gears.wallpaper.fit(config.get('wallpaper', theme.path() .. theme.get().wallpaper))
+
+for s = 1, screen.count() do
+	gears.wallpaper.fit(config.get('wallpaper', theme.path() .. theme.get().wallpaper), s)
+end
