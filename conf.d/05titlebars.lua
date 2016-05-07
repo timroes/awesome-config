00b2fd5475302fac6cf33f108a7db859fe6da720
Tim Roes <mail@timroes.de>
2016-05-07 23:01:11 +0200

    Scale titlebar to dpi of screen

--- conf.d/05titlebars.lua
+++ conf.d/05titlebars.lua
@@ -6,7 +6,8 @@ local colorbox = require("lunaconf.widgets.colorbox")
 
 local theme = lunaconf.theme.get()
 
-local titlebar_height = 30
+local titlebar_height = lunaconf.dpi.toScale(30)
+local color_indicator_size = lunaconf.dpi.toScale(10)
 
 local titlebars_enabled = true
 
@@ -27,16 +28,23 @@ lunaconf.keys.globals(
 
 client.connect_signal("manage", function(c, startup)
 
-	local client_status = colorbox.rect(10, 10, {
-		margin = (titlebar_height - 10) / 2
+	local client_status = colorbox.rect(color_indicator_size, color_indicator_size, {
+		margin = (titlebar_height - color_indicator_size) / 2
 	})
 
 	local icon = awful.titlebar.widget.iconwidget(c)
-	local margin_icon = wibox.layout.margin(icon, 8, 12, 5, 5)
+	local margin_icon = wibox.layout.margin(icon,
+			lunaconf.dpi.toScale(8),
+			lunaconf.dpi.toScale(12),
+			lunaconf.dpi.toScale(5),
+			lunaconf.dpi.toScale(5)
+		)
 
 	local center_layout = wibox.layout.fixed.horizontal()
 	center_layout:add(margin_icon)
-	center_layout:add(awful.titlebar.widget.titlewidget(c))
+	local title_widget = awful.titlebar.widget.titlewidget(c)
+	lunaconf.dpi.textbox(title_widget)
+	center_layout:add(title_widget)
 
 	local titlebar = wibox.layout.align.horizontal()
 	titlebar:set_left(client_status)
