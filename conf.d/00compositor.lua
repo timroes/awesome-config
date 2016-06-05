b73422c53057189de9c79ec595ce8ed508826419
Tim Roes <mail@timroes.de>
2016-06-05 15:24:34 +0200

    use shadow hint to disable shadow on some windows

--- conf.d/00compositor.lua
+++ conf.d/00compositor.lua
@@ -1,3 +1,21 @@
 local lunaconf = require('lunaconf')
+local awful = require('awful')
+
+local function set_shadow_hint(c)
+	local no_shadow
+	if awful.client.floating.get(c) then
+		no_shadow = 0
+	else
+		no_shadow = 1
+	end
+	c:set_xproperty("_COMPTON_NO_SHADOW", no_shadow)
+end
+
+-- Shadow handling of compton
+awesome.register_xproperty("_COMPTON_NO_SHADOW", "number")
+client.connect_signal("manage", function(c, startup)
+	set_shadow_hint(c)
+	c:connect_signal("property::floating", set_shadow_hint)
+end)
 
 lunaconf.utils.run_once('compton --config ' .. awful.util.getdir('config') .. '/compton.conf -b')
