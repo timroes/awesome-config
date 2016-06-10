a50cf38f992c699aae28c62b6f7deaaef916c2ef
Tim Roes <mail@timroes.de>
2016-06-10 17:11:42 +0200

    Improve dpi textbox handling

--- lib/lunaconf/dpi.lua
+++ lib/lunaconf/dpi.lua
@@ -1,5 +1,8 @@
 local theme = require('lunaconf.theme')
 local config = require('lunaconf.config')
+local wibox = require('wibox')
+
+local typeof = typeof
 
 -- Utilities to work with hidpi screens
 local dpi = {}
@@ -12,7 +15,13 @@ local scale = currentDpi / 96
 -- so it will take the dpi from the theme into respect. This method will assume
 -- the font size set on the textbox was meant to be for 96 dpi.
 function dpi.textbox(textbox)
+	if textbox == nil then
+		textbox = wibox.widget.textbox()
+	elseif type(textbox) == "string" then
+		textbox = wibox.widget.textbox(textbox)
+	end
 	textbox._layout:get_context():set_resolution(currentDpi)
+	return textbox
 end
 
 function dpi.toScale(value)
