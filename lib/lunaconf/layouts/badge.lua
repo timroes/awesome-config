d0e62acea35f95efc199b5f9b89af256dfb11974
Tim Roes <mail@timroes.de>
2016-06-11 18:16:57 +0200

    Improve performance of badge widget a bit

--- lib/lunaconf/layouts/badge.lua
+++ lib/lunaconf/layouts/badge.lua
@@ -9,8 +9,6 @@ local wibox = require('wibox')
 local badge = {}
 
 local function draw_badge(badge, wibox, cr, width, height, horizontal, vertical)
-	if not badge then return end
-
 	local w, h = base.fit_widget(badge.widget, width, height)
 	if badge.max_width then
 		w = math.min(width * badge.max_width, w)
@@ -28,10 +26,18 @@ end
 function badge:draw(wibox, cr, width, height)
 	base.draw_widget(wibox, cr, self.widget, 0, 0, width, height)
 
-	draw_badge(self.badges.se, wibox, cr, width, height, 1, 1)
-	draw_badge(self.badges.sw, wibox, cr, width, height, 0, 1)
-	draw_badge(self.badges.ne, wibox, cr, width, height, 1, 0)
-	draw_badge(self.badges.nw, wibox, cr, width, height, 0, 0)
+	if self.badges.se then
+		draw_badge(self.badges.se, wibox, cr, width, height, 1, 1)
+	end
+	if self.badges.sw then
+		draw_badge(self.badges.sw, wibox, cr, width, height, 0, 1)
+	end
+	if self.badges.ne then
+		draw_badge(self.badges.ne, wibox, cr, width, height, 1, 0)
+	end
+	if self.badges.nw then
+		draw_badge(self.badges.nw, wibox, cr, width, height, 0, 0)
+	end
 end
 
 function badge:add_badge(placement, widget, margin, max_width, max_height)
