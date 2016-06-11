49b3353201f141a88fb06d82408df3b46a302fc3
Tim Roes <mail@timroes.de>
2016-06-11 18:13:40 +0200

    don't instantiate internal launcher when not used

--- conf.d/70launcher.lua
+++ conf.d/70launcher.lua
@@ -1,6 +1,5 @@
 local lunaconf = require('lunaconf')
 
-local launcher = lunaconf.launcher()
 
 -- Bin dlauncher to MOD + space if a launcher has been defined in the configuration
 local ext_launcher = lunaconf.config.get('applications.launcher', nil)
@@ -9,6 +8,7 @@ if ext_launcher then
 		awful.key({ lunaconf.config.MOD }, "space", function() awful.util.spawn(ext_launcher) end)
 	)
 else
+	local launcher = lunaconf.launcher()
 	lunaconf.keys.globals(
 		awful.key({ lunaconf.config.MOD }, "space", function() launcher.toggle() end),
 		awful.key({ lunaconf.config.MOD }, "KP_Insert", function() launcher.toggle() end)
