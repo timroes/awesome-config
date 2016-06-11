6348f0f7c4e494f8e494b9a53096e5a5aaf5c7aa
Tim Roes <mail@timroes.de>
2016-06-11 18:29:33 +0200

    Update textclock only every 30 seconds
    Since updating any widget will still require a complete redraw of all drawables in awesome 3.5 it's better to not redraw the clock so often.

--- conf.d/99bar.lua
+++ conf.d/99bar.lua
@@ -84,7 +84,7 @@ for s = 1, screen.count() do
 			elseif w == 'systray' then
 				right_layout:add(wibox.widget.systray())
 			elseif w == 'clock' then
-				local clock = awful.widget.textclock("%a, %e. %b  %H:%M", 1)
+				local clock = awful.widget.textclock("%a, %e. %b  %H:%M", 30)
 				lunaconf.dpi.textbox(clock)
 				orglendar(clock)
 				right_layout:add(clock)
