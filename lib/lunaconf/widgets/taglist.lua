0f63c3a290e71612260b918ef13a43a6ce358523
Tim Roes <mail@timroes.de>
2016-06-15 11:45:28 +0200

    Remove debug logs

--- lib/lunaconf/widgets/taglist.lua
+++ lib/lunaconf/widgets/taglist.lua
@@ -1,13 +1,11 @@
 local awful = require('awful')
 local wibox = require('wibox')
 local common = require('lunaconf.widgets.common')
-local log = require('lunaconf.log')
 
 local tasklist = {}
 
 local function new(self, screen_index, filter, buttons)
 	local icon_widget_wrapper = function(...)
-		log.info("WHat type is screen? %s", screen_index)
 		common.icon_widgets(screen[screen_index], ...)
 	end
 	return awful.widget.taglist(screen_index, filter, buttons, nil, icon_widget_wrapper, wibox.layout.fixed.horizontal())
