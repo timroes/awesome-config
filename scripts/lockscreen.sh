a5dc3ea4ef59fd59e520805042d4f3feb8bd4d65
Tim Roes <mail@timroes.de>
2016-07-01 11:40:53 +0200

    Remove fancy lockscreen again (since it's buggy and slow)

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -1,14 +1,3 @@
 #! /bin/bash
 
-command -v import > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	# If imagemagick is installed, make a screenshot, pixelate it and show it as background
-	filename=`mktemp --suffix .png`
-	import -window root -colorspace gray $filename
-	# convert $filename -colorspace gray -blur 0x12 -auto-level +depth $filename
-	convert $filename -scale 4% -scale 2500% $filename
-	i3lock -i "$filename" -d
-	rm $filename
-else
-	i3lock -c BBBBBB -d
-fi
+i3lock -c BBBBBB -d
