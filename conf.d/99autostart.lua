74d44607c807febaef6f960b0cf72f1bf7ea171c
Tim Roes <mail@timroes.de>
2017-04-13 08:16:35 +0200

    Add icon to dex missing warning

--- conf.d/99autostart.lua
+++ conf.d/99autostart.lua
@@ -1,15 +1,15 @@
 local lunaconf = require('lunaconf')
-local naughty = require('naughty')
 
 -- Use dex tool to start all desktop files from xdg autostart folders or show a warning
 -- if dex isn't installed
 lunaconf.utils.command_exists('dex', function(exists)
 	if exists then
-		lunaconf.utils.spawn("dex -a -e awesome")
+		lunaconf.utils.spawn('dex -a -e awesome')
 	else
-		naughty.notify({
-			title = "dex missing",
-			text = "Install dex to enable autostart"
+		lunaconf.notify.show({
+			title = 'dex missing',
+			text = 'Install `dex` to enable autostart',
+			icon = 'dialog-warning'
 		})
 	end
 end)
