20e4f25d55401e39195efcf814c064204144c913
Tim Roes <tim.roes@inovex.de>
2017-05-11 15:54:57 +0200

    Restore hidden clients on their previous screen

--- conf.d/05hidden_clients.lua
+++ conf.d/05hidden_clients.lua
@@ -14,6 +14,7 @@ local hidden_count_textbox = nil
 -- Hide the currently focused client.
 local function hide_client()
 	if not client.focus then return end
+	client.focus.screen_before_hiding = client.focus.screen
 	client.focus:move_to_tag(hidden_tag)
 end
 
@@ -22,7 +23,11 @@ end
 local function unhide_all_clients()
 	local hidden_clients = hidden_tag:clients()
 	for i, client in ipairs(hidden_clients) do
-		client:move_to_tag(client.screen.primary_tag)
+		if client.screen_before_hiding and client.screen_before_hiding.valid then
+			client:move_to_tag(client.screen_before_hiding.primary_tag)
+		else
+			client:move_to_tag(lunaconf.screens.primary().primary_tag)
+		end
 	end
 end
 
