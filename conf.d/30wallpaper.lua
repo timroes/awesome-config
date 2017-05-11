a36687419cbcd58d7271eed8c4c86058449c5566
Tim Roes <tim.roes@inovex.de>
2017-05-11 13:02:36 +0200

    Also reset wallpaper if screen geometry changes

--- conf.d/30wallpaper.lua
+++ conf.d/30wallpaper.lua
@@ -2,6 +2,10 @@ local awful = require('awful')
 local gears = require('gears')
 local lunaconf = require('lunaconf')
 
-awful.screen.connect_for_each_screen(function(s)
+local function set_wallpaper(s)
 	gears.wallpaper.tiled(lunaconf.config.get('theme.wallpaper', nil), s)
-end)
+end
+
+-- Set wallpaper for each new screen and if the geometry of a screen changes
+awful.screen.connect_for_each_screen(set_wallpaper)
+screen.connect_signal('property::geometry', set_wallpaper)
