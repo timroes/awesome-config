ff885137e6af23f5a861c0285706ebeaa4e92f6c
Tim Roes <tim.roes@inovex.de>
2017-05-11 12:45:44 +0200

    Make bars work when screens change without restart

--- conf.d/89bar.lua
+++ conf.d/89bar.lua
@@ -9,9 +9,9 @@ local gears = require('gears')
 local lunaconf = require('lunaconf')
 local primary_screen = lunaconf.screens.primary_index()
 
-bars = {}
-taglist = {}
-tasklists = {}
+local bars = {}
+local taglist = {}
+local tasklists = {}
 
 taglist.buttons = awful.util.table.join(
 	awful.button({ }, 1, awful.tag.viewonly)
@@ -44,7 +44,9 @@ tasklists.buttons = awful.util.table.join(
 	end)
 )
 
-local spacer = function(width, screen)
+--- A simple function creating a spacer widget with the given with for the specified
+--- screen.
+local function spacer(width, screen)
 	return wibox.widget.textbox(string.rep(" ", math.floor(lunaconf.dpi.x(width, screen))))
 end
 
@@ -56,8 +58,42 @@ local filter_noname_tags = function(t)
 	return t.name:len() <= 1
 end
 
-for s = 1, screen.count() do
+--- Create the widgets, that should only be shown on the primary screen.
+-- This will read the config option 'bar.widgets' and creates widgets acoordingly
+-- to this config.
+local function create_primaryscreen_widgets()
+	local primary = lunaconf.screens.primary()
+	local primaryscreen_widgets = wibox.layout.fixed.horizontal()
 
+	-- load widgets from config file
+	local widgets = lunaconf.strings.split(config.get('bar.widgets', ''), ',')
+	for i,w in pairs(widgets) do
+		if tonumber(w) ~= nil and tonumber(w) > 0 then
+			primaryscreen_widgets:add(spacer(tonumber(w), primary))
+		elseif w == 'displayswitcher' then
+			primaryscreen_widgets:add(displayswitcher())
+		elseif w == 'battery' then
+			-- TODO: Broken in awesome 4, create a new one
+			primaryscreen_widgets:add(battery(primary))
+		elseif w == 'screensaver' then
+			primaryscreen_widgets:add(lunaconf.widgets.screensaver(primary))
+		elseif w == 'network' then
+			primaryscreen_widgets:add(lunaconf.widgets.networkmonitor())
+		elseif w == 'systray' then
+			primaryscreen_widgets:add(wibox.widget.systray())
+		elseif w == 'clock' then
+			local clock = wibox.widget.textclock("%a, %e. %b  %H:%M")
+			lunaconf.dpi.textbox(clock, primary)
+			orglendar(clock, primary)
+			primaryscreen_widgets:add(clock)
+		end
+	end
+
+	return primaryscreen_widgets
+end
+
+-- For all current and future screens create a bar
+awful.screen.connect_for_each_screen(function(s)
 	-- TODO: Use custom tasklist again if needed
 	tasklists[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklists.buttons)
 
@@ -68,39 +104,9 @@ for s = 1, screen.count() do
 	left_layout:add(awful.widget.taglist(s, function(t) return not t.invisible end, taglist.buttons))
 	left_layout:add(spacer(5, screen[s]))
 
-	local right_layout = wibox.layout.fixed.horizontal()
-	if s == primary_screen then
-
-		-- load widgets from config file
-		local widgets = lunaconf.strings.split(config.get('bar.widgets', ''), ',')
-		for i,w in pairs(widgets) do
-			if tonumber(w) ~= nil and tonumber(w) > 0 then
-				right_layout:add(spacer(tonumber(w), screen[s]))
-			elseif w == 'displayswitcher' then
-				right_layout:add(displayswitcher())
-			elseif w == 'battery' then
-				-- TODO: Broken in awesome 4, create a new one
-				right_layout:add(battery(screen[s]))
-			elseif w == 'screensaver' then
-				right_layout:add(lunaconf.widgets.screensaver(screen[s]))
-			elseif w == 'network' then
-				right_layout:add(lunaconf.widgets.networkmonitor())
-			elseif w == 'systray' then
-				right_layout:add(wibox.widget.systray())
-			elseif w == 'clock' then
-				local clock = wibox.widget.textclock("%a, %e. %b  %H:%M")
-				lunaconf.dpi.textbox(clock, screen[s])
-				orglendar(clock, screen[s])
-				right_layout:add(clock)
-			end
-		end
-
-	end
-
 	local layout = wibox.layout.align.horizontal()
 	layout:set_left(left_layout)
 	layout:set_middle(tasklists[s])
-	layout:set_right(right_layout)
 
 	bars[s] = awful.wibar({
 		position = "top",
@@ -109,8 +115,26 @@ for s = 1, screen.count() do
 		bg = lunaconf.theme.get().screenbar_bg
 	})
 	bars[s]:set_widget(layout)
+end)
 
+--- Function to be executed whenever the primary screen changes.
+-- This will remove the primar widgets from all bars (actually it should only be
+-- on the old primary screen) and after that creates a new set of widgets for the
+-- new primary screen and attach them to its bar.
+local function update_primary_bar()
+	-- Remove primary widgets from all current screens.
+	for s in screen do
+		bars[s].widget:set_right(nil)
+	end
+	-- Create new widgets and attach them on the current primary
+	local primary_bar = bars[lunaconf.screens.primary()]
+	primary_bar.widget:set_right(create_primaryscreen_widgets())
 end
+
+-- Whenever the primary change move the widgets to the new primary bar
+screen.connect_signal('primary_changed', update_primary_bar)
+-- Initialize the widgets on the current primary
+update_primary_bar()
 
 local bars_visible = true
 
