684c5cfe1d4a201f0f0c0a5efe13e0e714ca884b
Tim Roes <mail@timroes.de>
2017-06-05 17:18:47 +0200

    Improve compton config
    Handle shaped window also inside awesome to prevent notification
    from having no shadow

--- conf.d/00compositor.lua
+++ conf.d/00compositor.lua
@@ -4,16 +4,17 @@ local gears = require('gears')
 
 if not lunaconf.config.get('disable_compositor', false) then
 	local function set_shadow_hint(c)
-		c:set_xproperty("_COMPTON_NO_SHADOW", not c.floating)
+		c:set_xproperty("_COMPTON_NO_SHADOW", not c.floating or c.shape_client_bounding)
 	end
 
 	-- Shadow handling of compton
 	-- Disable shadows (set _COMPTON_NO_SHADOW xproperty) on all non floating windows
-	-- so they won't leave ugly shadows on the screen bar(s).
-	awesome.register_xproperty("_COMPTON_NO_SHADOW", "boolean")
-	client.connect_signal("manage", function(c, startup)
+	-- and windows with a shape so they won't leave ugly shadows on the screen bar(s).
+	awesome.register_xproperty('_COMPTON_NO_SHADOW', 'boolean')
+	client.connect_signal('manage', function(c, startup)
 		set_shadow_hint(c)
-		c:connect_signal("property::floating", set_shadow_hint)
+		c:connect_signal('property::floating', set_shadow_hint)
+		c:connect_signal('property::shape_client_bounding', set_shadow_hint)
 	end)
 
 	lunaconf.utils.only_if_command_exists('compton', function()
