cb74b9927c1305545a8fa3a626a6c423460975dc
Tim Roes <mail@timroes.de>
2017-06-05 21:50:59 +0200

    Remove old unused widgets

--- lib/lunaconf/widgets/init.lua
+++ lib/lunaconf/widgets/init.lua
@@ -3,11 +3,9 @@ return {
 	colorbox = require('lunaconf.widgets.colorbox'),
 	common = require('lunaconf.widgets.common'),
 	dnd = require('lunaconf.widgets.dnd'),
-	networkmonitor = require('lunaconf.widgets.networkmonitor'),
 	screensaver = require('lunaconf.widgets.screensaver'),
 	systray = require('lunaconf.widgets.systray'),
 	taglist = require('lunaconf.widgets.taglist'),
 	tasklist = require('lunaconf.widgets.tasklist'),
-	textclock = require('lunaconf.widgets.textclock'),
 	titlebar = require('lunaconf.widgets.titlebar')
 }
