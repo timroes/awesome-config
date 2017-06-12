8a22cc90b83c5e8fcd7b9aceda893c02632c8eb3
Tim Roes <mail@timroes.de>
2017-06-12 23:29:18 +0200

    Refactor screensaver file

--- conf.d/60screensaver.lua
+++ conf.d/60screensaver.lua
@@ -7,8 +7,10 @@ local lock_cmd = lunaconf.utils.scriptpath() .. "lockscreen.sh"
 -- lockout time in minutes
 local screensaver_timeout = lunaconf.config.get('screensaver.timeout', 10)
 
--- Use lock_cmd as a screensaver (requires x11-misc/xautolock)
+-- Use lock_cmd as a screensaver
 lunaconf.utils.run_once("xautolock -time " .. screensaver_timeout .. " -locker '" .. lock_cmd .. "'")
 
 -- Add shortcut for config.MOD + l to lock the screen
-lunaconf.keys.globals(awful.key({ config.MOD }, "l", function() awful.spawn.spawn(lock_cmd) end))
+lunaconf.keys.globals(awful.key({ config.MOD }, "l", function()
+	awful.spawn.spawn('xautolock -locknow')
+end))
