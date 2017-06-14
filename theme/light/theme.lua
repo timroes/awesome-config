78af159490230a6b66c439e2f537765655f77360
Tim Roes <mail@timroes.de>
2017-06-14 22:51:34 +0200

    Remove old linear colors from theme

--- theme/light/theme.lua
+++ theme/light/theme.lua
@@ -4,8 +4,8 @@ local theme = {}
 theme.font          = "Roboto Light 11"
 
 -- theme.bg_normal     = "linear:0,0:0,28:0,#3A3A3A:1,#202020"
-theme.bg_normal = "#F5F5F5AA"
-theme.bg_focus      = "linear:0,0:0,28:0,#BEBEBE:1,#EEEEEE"
+theme.bg_normal     = "#F5F5F5AA"
+theme.bg_focus      = "#BEBEBE"
 theme.bg_urgent     = "linear:0,0:0,28:0,#99CC00:1,#739900"
 theme.bg_minimize   = "#111111"
 
