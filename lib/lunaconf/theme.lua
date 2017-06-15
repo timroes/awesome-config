8064e76a8cf74aab94a568eee9fca860b0d1c95e
Tim Roes <mail@timroes.de>
2017-06-15 14:39:22 +0200

    Cleanup lunaconf.theme

--- lib/lunaconf/theme.lua
+++ lib/lunaconf/theme.lua
@@ -4,16 +4,10 @@ local gears = require('gears')
 
 local theme = {}
 
-local themepath = gears.filesystem.get_configuration_dir() .. "/theme/light/"
-
 beautiful.init(gears.filesystem.get_configuration_dir() .. "/theme/light/theme.lua")
 
 function theme.get()
 	return beautiful.get()
-end
-
-function theme.path()
-	return themepath
 end
 
 return theme
