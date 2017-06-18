1db4cee9305ecc8803bca2c40c6ba4d2036dc62d
Tim Roes <mail@timroes.de>
2017-06-18 18:55:01 +0200

    Fix tilda config

--- conf.d/51tilda.lua
+++ conf.d/51tilda.lua
@@ -1,21 +1,29 @@
 local awful = require('awful')
+local gears = require('gears')
 local lunaconf = require('lunaconf')
-local TILDA_SCREEN = lunaconf.screens.primary_index()
+
+local function set_position(c)
+	local workarea = c.screen.workarea
+	c:geometry(workarea)
+end
 
 -- Scale window to workspace size (min 15% height)
 client.connect_signal("manage", function(c, startup)
 	if c.class == "Tilda" then
-		local wa = screen[TILDA_SCREEN].workarea
-		c:geometry({
-			x = wa.x,
-			y = wa.y,
-			width = wa.width,
-			height = wa.height * 0.85
-		})
+		c.screen = lunaconf.screens.primary()
+
+		-- Disable shadow for tilda clients
+		c:set_xproperty("_COMPTON_NO_SHADOW", true)
+
+		-- Set property of tilda whenever it changes (prevent its own scaling mechanism)
+		c:connect_signal('property::geometry', set_position)
+		-- Fix geometry when the screen of tilda changes
+		c:connect_signal('property::screen', set_position)
+		set_position(c)
 	end
 end)
 
-awful.rules.rules = awful.util.table.join(awful.rules.rules, {
+awful.rules.rules = gears.table.join(awful.rules.rules, {
 	{
 		rule = { class = "Tilda" },
 		properties = { floating = true, border_width = 0, opacity = 0.9 }
