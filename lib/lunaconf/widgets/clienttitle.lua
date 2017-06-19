6006c8be759bddcc0e81ca3a1612b0a2dcd2bd53
Tim Roes <mail@timroes.de>
2017-06-19 13:21:21 +0200

    Allow focusing and closing via clientitle

--- lib/lunaconf/widgets/clienttitle.lua
+++ lib/lunaconf/widgets/clienttitle.lua
@@ -1,18 +1,46 @@
 local awful = require('awful')
+local gears = require('gears')
 local wibox = require('wibox')
 
 local clienttitle = {}
 
 local function new(_, screen)
+
+	local get_client = function()
+		return awful.client.focus.history.get(screen, 0)
+	end
+
 	local textbox = wibox.widget.textbox()
 	local update_client = function()
-		local c = awful.client.focus.history.get(screen, 0)
+		local c = get_client()
 		if c and c.name then
 			textbox.text = c.name
 		else
 			textbox.text = ''
 		end
 	end
+
+	local focus_client = function()
+		local c = get_client()
+		if c then
+			client.focus = c
+		end
+	end
+
+	local kill_client = function()
+		local c = get_client()
+		if c then
+			c:kill()
+		end
+	end
+
+	-- Setup buttons
+	local buttons = gears.table.join(
+		awful.button({ }, 1, focus_client),
+		awful.button({ }, 2, kill_client)
+	)
+
+	textbox:buttons(buttons)
 	client.connect_signal('property::name', update_client)
 	client.connect_signal('focus', update_client)
 	client.connect_signal('unfocus', update_client)
