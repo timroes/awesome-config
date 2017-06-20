581ad70e4a0f0dbc28f9db46fcbd823ed4510d83
Tim Roes <mail@timroes.de>
2017-06-20 23:38:05 +0200

    Automatically reset disconnected screens

--- scripts/displayswitcher.py
+++ scripts/displayswitcher.py
@@ -44,7 +44,7 @@ def display_infos():
         'height': int(size.group('height'))
       }
 
-    if resolution and size:
+    if resolution and size and display['size_mm']['width'] > 0 and display['size_mm']['height'] > 0:
       display['dpi'] = {
         'x': round((display['resolution']['x'] * 25.4) / display['size_mm']['width']),
         'y': round((display['resolution']['y'] * 25.4) / display['size_mm']['height'])
@@ -52,24 +52,34 @@ def display_infos():
 
     displays.append(display)
 
-  return displays
+  return {
+    'connected': [x for x in displays if x['state'] == 'connected'],
+    'disconnected': [x for x in displays if x['state'] == 'disconnected']
+  }
 
-def connected_displays():
-  infos = display_infos()
-  return [x for x in infos if x['state'] == 'connected']
+def off_all_disconnected(disconnected):
+  args = []
+  for display in disconnected:
+    args.extend(['--output', display['id'], '--off'])
+  return args
 
 def layout_extend():
   '''
     Extends layout chains all displays right-of the previous display and give
     them their auto resolution.
   '''
-  displays = connected_displays()
+  displays = display_infos()
 
-  if len(displays) > 1:
-    args = ['xrandr', '--output', displays[0]['id'], '--auto']
+  if len(displays['connected']) > 0:
+    args = [
+        'xrandr', '--output', displays['connected'][0]['id'],
+        '--auto', '--transform', '1,0,0,0,1,0,0,0,1']
     # Only setup layout if there is more than one display
-    for i, display in enumerate(displays[1:], start=1):
-      args.extend(['--output', display['id'], '--auto', '--right-of', displays[i-1]['id']])
+    for i, display in enumerate(displays['connected'][1:], start=1):
+      args.extend(['--output', display['id'], '--auto', '--right-of', displays['connected'][i-1]['id']])
+
+    # Switch all disconnected displays off
+    args.extend(off_all_disconnected(displays['disconnected']))
 
     subprocess.call(args)
 
@@ -77,27 +87,29 @@ def layout_clone():
   '''
     Clone layout will make all outputs show the same picture.
   '''
-  displays = connected_displays()
+  displays = display_infos()
 
   # Find the display with the lowest preferred resolution
-  min_res_display = min(displays, key=lambda d: d['preferred']['x'] * d['preferred']['y'])
+  min_res_display = min(displays['connected'], key=lambda d: d['preferred']['x'] * d['preferred']['y'])
   min_resolution = '{}x{}'.format(min_res_display['preferred']['x'], min_res_display['preferred']['y'])
 
-  if len(displays) > 1:
+  if len(displays['connected']) > 1:
     args = ['xrandr', '--output', min_res_display['id'], '--auto']
 
-    for display in filter(lambda d: d['id'] != min_res_display['id'], displays):
+    for display in filter(lambda d: d['id'] != min_res_display['id'], displays['connected']):
       pref_res = '{}x{}'.format(display['preferred']['x'], display['preferred']['y'])
       args.extend(['--output', display['id'], '--same-as', min_res_display['id'],
         '--mode', pref_res, '--scale', pref_res, '--scale-from', min_resolution])
 
-    # print(' '.join(args))
+    # Switch all disconnected displays off
+    args.extend(off_all_disconnected(displays['disconnected']))
+
     subprocess.call(args)
 
 def print_state():
-  infos = connected_displays()
-  print(', '.join([x['id'] for x in infos]), end='')
-  if len(infos) > 1:
+  infos = display_infos()
+  print(', '.join([x['id'] for x in infos['connected']]), end='')
+  if len(infos['connected']) > 1:
     sys.exit(0)
   else:
     # If there is only one display connected exit with exit code 26
@@ -105,11 +117,9 @@ def print_state():
 
 if __name__ == '__main__':
   what = '' if len(sys.argv) < 2 else sys.argv[1]
-  if what == 'auto':
-    pass
-  elif what == 'clone':
+  if what == 'clone':
     layout_clone()
-  elif what == 'extend':
+  elif what == 'extend' or what == 'auto':
     layout_extend()
   else:
     print_state()
