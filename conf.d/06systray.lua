25856904c585e61d73205bb7d5ad8381cddd4d33
Tim Roes <mail@timroes.de>
2017-06-24 09:02:48 +0200

    Remove unnecessary files

--- conf.d/06systray.lua
+++
@@ -1,7 +0,0 @@
-local lunaconf = require('lunaconf')
-local awful = require('awful')
-
--- local systray = lunaconf.widgets.systray()
--- lunaconf.keys.globals(
-	-- awful.key({ lunaconf.config.MOD }, "/", function() systray:toggle() end)
--- )
