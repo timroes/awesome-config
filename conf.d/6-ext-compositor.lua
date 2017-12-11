4516c94d780866abda4a82f96bbf5691d8dc9bf1
Tim Roes <mail@timroes.de>
2017-12-11 22:41:21 +0100

    Restart compton when screen configuration changes

--- conf.d/6-ext-compositor.lua
+++ conf.d/6-ext-compositor.lua
@@ -2,6 +2,10 @@ local lunaconf = require('lunaconf')
 local awful = require('awful')
 local gears = require('gears')
 
+local run_compton = function()
+	lunaconf.utils.run_once('compton --config ' .. gears.filesystem.get_configuration_dir() .. '/configs/compton.conf -b')
+end
+
 if not lunaconf.config.get('disable_compositor', false) then
 	local function set_shadow_hint(c)
 		c:set_xproperty("_COMPTON_NO_SHADOW", not c.floating)
@@ -17,8 +21,15 @@ if not lunaconf.config.get('disable_compositor', false) then
 	end)
 
 	lunaconf.utils.only_if_command_exists('compton', function()
+		-- Due to a bug in compton we need to kill and restart it if the screen
+		-- configuration changes, since otherwise some screens might stay blank
+		screen.connect_signal('list', function()
+			lunaconf.utils.spawn('killall compton')
+			run_compton()
+		end)
+
 		if not awesome.composite_manager_running then
-			lunaconf.utils.run_once('compton --config ' .. gears.filesystem.get_configuration_dir() .. '/configs/compton.conf -b')
+			run_compton()
 		end
 	end)
 end
