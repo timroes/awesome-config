50f29545921f7cfa73afcc87f28aa0e452b2f1ed
Tim Roes <mail@timroes.de>
2020-08-09 19:32:08 +0200

    Indicate screen number via dot indicator

--- lib/lunaconf/tags/widget.lua
+++ lib/lunaconf/tags/widget.lua
@@ -2,16 +2,38 @@ local awful = require('awful')
 local gears = require('gears')
 local wibox = require('wibox')
 local lunaconf = {
-	config = require('lunaconf.config'),
 	dpi = require('lunaconf.dpi'),
-	notify = require('lunaconf.notify'),
-	theme = require('lunaconf.theme'),
-	utils = require('lunaconf.utils')
+	log = require('lunaconf.log'),
+	theme = require('lunaconf.theme')
 }
 
 local tagwidget = {}
 
 local theme = lunaconf.theme.get()
+
+local dot_indicators = {
+	{ { 0, 0 } },
+	{ { -0.35, 0 }, { 0.35, 0 } },
+	{ { 0, -0.35}, { -0.35, 0.35 }, { 0.35, 0.35 } },
+	{ { -0.35, -0.35 }, { 0.35, -0.35 }, { 0.35, 0.35 }, { -0.35, 0.35} }
+}
+
+local function draw_position_indicator(cr, screen, position, size)
+	if position > #dot_indicators then
+		return lunaconf.log.err('The tag widget only supports up to ' .. tostring(#dot_indicators) .. ' screens.')
+	end
+
+	local dot_size = lunaconf.dpi.x(4, screen)
+	cr:set_source_rgb(gears.color.parse_color(theme.tag_color_fg))
+
+	for _, dot in ipairs(dot_indicators[position]) do
+		cr:save()
+		cr:translate(dot[1] * size - dot_size / 2, dot[2] * size - dot_size / 2)
+		gears.shape.circle(cr, dot_size, dot_size)
+		cr:fill()
+		cr:restore()
+	end
+end
 
 local function new(self, screen, tags, args)
 	self = {
@@ -40,14 +62,13 @@ local function new(self, screen, tags, args)
 					:translate(0.2 * height + (i-1) * height - (i-1) * height * 0.2, 0.2 * height)
 					(cr, 0.6 * height, 0.6 * height, self._dx(3))
 				cr:fill()
-				-- TODO: Add screen position number to active tag
-				-- if i == self._selected_tag then
-				-- 	cr:set_font_size(height / 1.5)
-				-- 	local extents = cr:text_extents(tostring(position))
-				-- 	cr:set_source_rgb(gears.color.parse_color(theme.tag_color_fg))
-				-- 	cr:move_to(width / 2 - extents.width / 2, height / 2 + extents.height / 2)
-				-- 	cr:show_text(tostring(screen.position))
-				-- end
+
+				if i == self._selected_tag then
+					cr:save()
+					cr:translate(0.5 * height + (i-1) * height - (i-1) * height * 0.2, 0.5 * height)
+					draw_position_indicator(cr, screen, screen.position, height * 0.6 / 2)
+					cr:restore()
+				end
 			end
 		end
 	}
