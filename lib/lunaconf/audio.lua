552fb9154a878419791243ceb3aa1ed7b2e3bfc6
Tim Roes <mail@timroes.de>
2021-12-30 17:18:48 +0100

    Fix mute logic

--- lib/lunaconf/audio.lua
+++ lib/lunaconf/audio.lua
@@ -1,11 +1,17 @@
 -- A module to offer helper to control volume, etc.
 -- This module requires alsa-utils to be installed on the system.
 local awful = require('awful')
+local lunaconf = {
+	config = require('lunaconf.config'),
+	notify = require('lunaconf.notify')
+}
 
 local tonumber = tonumber
 local tostring = tostring
 
 local audio = {}
+
+local additional_masters = lunaconf.config.get('audio.additional_master_channels', nil)
 
 local function parse_amixer_output(output)
 	local is_muted = output:find('%[on%]') == nil
@@ -28,9 +34,20 @@ end
 -- and the volume level (between 0 and 100) as second parameter.
 function audio.toggle_mute(callback)
 	awful.spawn.easy_async('amixer set Master toggle', function(stdout)
+		local is_muted, volume = parse_amixer_output(stdout)
+		-- If we're unmuting we also unmute additional master channels that have
+		-- been specified in the config. This is a workaround for amixer not unmuting
+		-- e.g. Speaker on some machines after unmuting Master.
+		-- see https://bugs.launchpad.net/ubuntu/+source/alsa-utils/+bug/1026331
+		-- see https://bugs.launchpad.net/ubuntu/+source/pulseaudio/+bug/878986
+		if additional_masters and not is_muted then
+			for _,m in ipairs(additional_masters) do
+				awful.spawn.spawn('amixer set ' .. m .. ' on')
+			end
+		end
 		-- If a callback has been specified inform whether sound is no on or off
 		if callback then
-			callback(parse_amixer_output(stdout))
+			callback(is_unmuted, volume)
 		end
 	end)
 end
