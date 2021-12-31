508bf262d792a643c0abcf1cf3f396f36a125862
Tim Roes <mail@timroes.de>
2021-12-31 21:07:53 +0100

    Improve screen locker code

--- src/conf/screenlock.ts
+++ src/conf/screenlock.ts
@@ -3,8 +3,9 @@ import { SCRIPT_PATH, SUPER } from '../lib/constants';
 import { addKey } from '../lib/keys';
 import { spawn, spawn_once } from '../lib/process';
 
-const screensaver_timeout = lunaconf.config.get('screensaver.timeout', 10);
+const screensaverTimeout = lunaconf.config.get('screensaver.timeout', 10);
+const lockCommand = `${SCRIPT_PATH}/lockscreen.sh`;
 
-spawn_once(`xautolock -time ${screensaver_timeout} -locker ${SCRIPT_PATH}/lockscreen.sh`);
+spawn_once(`xautolock -time ${screensaverTimeout} -locker ${lockCommand}`);
 
-addKey([SUPER], 'l', () => spawn('xautolock -locknow'));
+addKey([SUPER], 'l', () => spawn(lockCommand));
