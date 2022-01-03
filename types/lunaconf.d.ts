30a660be4dd2bbc561cad1e521a0030d0648c117
Tim Roes <mail@timroes.de>
2022-01-03 22:36:41 +0100

    Complete lunaconf.dialogs.bar typings

--- types/lunaconf.d.ts
+++ types/lunaconf.d.ts
@@ -11,6 +11,8 @@ declare module 'lunaconf' {
   
   interface BarDialogInstance {
     set_value(value: number): void;
+    set_disabled(disabled: boolean): void;
+    set_icon(icon: string): void;
     show(): void;
   }
 
