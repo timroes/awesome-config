cd75125b9b23ceab4444c1dd14ffc2847cd3b588
Tim Roes <mail@timroes.de>
2023-10-15 20:28:15 -0700

    Exclude trackpoint from left handed

--- scripts/pointing-devices.sh
+++ scripts/pointing-devices.sh
@@ -19,11 +19,17 @@ if [ "$touchpad" ]; then
 fi
 
 # Make external mouse left handed
-xinput list --id-only | while IFS= read -r line; do
-	left_handed="$(xinput list-props $line | grep -i "left handed enabled (")"
+xinput list | while IFS= read -r line; do
+	# Do not switch buttons for the Thinkpad TrackPoint
+	if [[ "${line,,}" == *"trackpoint"* ]]; then
+		continue;
+	fi
+	[[ "$line" =~ "id="([0-9]+) ]]
+	device="${BASH_REMATCH[1]}"
+	left_handed="$(xinput list-props $device | grep -i "left handed enabled (")"
 	if [[ "$left_handed" ]]; then
 		[[ ${left_handed,,} =~ "left handed enabled ("([0-9]+)")" ]]
 		logger -t $TAG "Switching input device $line to left handed"
-		xinput set-prop $line ${BASH_REMATCH[1]} 1
+		xinput set-prop $device ${BASH_REMATCH[1]} 1
 	fi
 done
